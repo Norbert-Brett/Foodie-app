6a935bcf992d8145ff833af76ad5818b3a75b486
br3tt <roco_norbi@yahoo.com>
2022-11-13 10:48:12 +0000

    bottom navigation added

--- app/src/main/java/com/example/foodie_app/MainActivity.java
+++ app/src/main/java/com/example/foodie_app/MainActivity.java
@@ -4,11 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    RestaurantFragment restaurantFragment = new RestaurantFragment();
+    CateringFragment cateringFragment = new CateringFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    StreetFoodFragment streetFoodFragment = new StreetFoodFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.profile:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, profileFragment).commit();
+                    return true;
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, restaurantFragment).commit();
+                    return true;
+                case R.id.catering:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, cateringFragment).commit();
+                    return true;
+                case R.id.street_food:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, streetFoodFragment).commit();
+                    return true;
+            }
+            return false;
+        });
     }
 }
